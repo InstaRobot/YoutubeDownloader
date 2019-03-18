5e2f78c3b91a873205ae3a5a52c687aa4cff31a2
Khoa Pham <onmyway133@gmail.com>
2019-03-18 14:51:06 +0100

    Add test

--- YoutubeDownloaderTests/YoutubeDownloaderTests.swift
+++ YoutubeDownloaderTests/YoutubeDownloaderTests.swift
@@ -10,27 +10,10 @@ import XCTest
 @testable import YoutubeDownloader
 
 class YoutubeDownloaderTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+  func testFindName() {
+    let worker = Worker()
+    let string = "[download] Destination: /Users/khoa/Library/Containers/com.fantageek.YoutubeDownloader/Data/Downloads/Police - how to catch the thief.mp4"
+    let name = worker.findName(text: string)
+    XCTAssertEqual(name, "Police - how to catch the thief.mp4")
+  }
 }
